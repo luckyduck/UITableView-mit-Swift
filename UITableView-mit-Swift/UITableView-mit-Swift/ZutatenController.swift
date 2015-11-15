de98d6256fd61b7da431dfee7fc1e84b5a7a29b9
Jan Brinkmann <jan@code-x.de>
2015-11-15 21:04:57 +0100

    24_searchbar

--- UITableView-mit-Swift/UITableView-mit-Swift/ZutatenController.swift
+++ UITableView-mit-Swift/UITableView-mit-Swift/ZutatenController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ZutatenController: UITableViewController {
+class ZutatenController: UITableViewController, UISearchBarDelegate {
 
+    var isSearching = false
+    
     var rezept: Rezept?
     var zutaten = [String]() {
         didSet {
@@ -17,6 +19,9 @@ class ZutatenController: UITableViewController {
         }
     }
     
+    var filterZutaten = [String]()
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var editToggle: UIBarButtonItem!
     
     override func viewDidLoad() {
@@ -31,13 +36,21 @@ class ZutatenController: UITableViewController {
     // MARK: - Table view data source
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        if isSearching {
+            return filterZutaten.count
+        }
+        
         return zutaten.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("zutatCell", forIndexPath: indexPath)
 
-        cell.textLabel?.text = zutaten[indexPath.row]
+        if isSearching {
+            cell.textLabel?.text = filterZutaten[indexPath.row]
+        } else {
+            cell.textLabel?.text = zutaten[indexPath.row]
+        }
         
         return cell
     }
@@ -94,5 +107,42 @@ class ZutatenController: UITableViewController {
                 
             }
         }
+    }
+    
+    //
+    //
+    // searchbar delegate
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        isSearching = true;
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        isSearching = false;
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        isSearching = false;
+    }
+
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        isSearching = false;
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filterZutaten = zutaten.filter({
+            (zutat: String) -> Bool in
+            
+            return zutat.containsString(searchText)
+        })
+        
+        if filterZutaten.count == 0 {
+            isSearching = false
+        } else {
+            isSearching = true
+        }
+        
+        self.tableView.reloadData()
     }
 }
