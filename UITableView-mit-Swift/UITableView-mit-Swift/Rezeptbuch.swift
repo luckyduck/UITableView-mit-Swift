d5f785351c3930c4a059b470b4e21da88ae55e79
Jan Brinkmann <jan@code-x.de>
2015-11-15 10:22:32 +0100

    initial

---
+++ UITableView-mit-Swift/UITableView-mit-Swift/Rezeptbuch.swift
@@ -0,0 +1,38 @@
+//
+//  Rezeptbuch.swift
+//  UITableView-mit-Swift
+//
+//  Created by Jan Brinkmann on 15/11/15.
+//  Copyright © 2015 Jan Brinkmann. All rights reserved.
+//
+
+import Foundation
+
+
+/*
+Image credits:
+
+1) pasta
+https://www.flickr.com/photos/nebulux/7148182009
+by Luca Nebuloni
+License: CC Atribution 2.0 Generic
+https://creativecommons.org/licenses/by/2.0/
+
+2) pancakes
+https://www.flickr.com/photos/59247791@N08/5505408276/
+by rob_rob2001
+License: CC Attribution-ShareAlike 2.0 Generic
+https://creativecommons.org/licenses/by-sa/2.0/
+
+3) scrambledeggs
+https://www.flickr.com/photos/stevendepolo/4298287138
+by Steven Depolo
+License: CC Atribution 2.0 Generic
+https://creativecommons.org/licenses/by/2.0/
+
+4) salat
+https://www.flickr.com/photos/maltehempel_de/18937812818
+by Malte Hempel
+License: CC Attribution-ShareAlike 2.0 Generic
+https://creativecommons.org/licenses/by-sa/2.0/
+*/
