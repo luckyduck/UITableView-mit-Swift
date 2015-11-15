293316144c703e689ede1e41b15844b45959004a
Jan Brinkmann <jan@code-x.de>
2015-11-15 15:14:31 +0100

    14_eigene-uitableviewcell 15_accessorytype

---
+++ UITableView-mit-Swift/UITableView-mit-Swift/RezeptCell.swift
@@ -0,0 +1,24 @@
+//
+//  RezeptCell.swift
+//  UITableView-mit-Swift
+//
+//  Created by Jan Brinkmann on 15/11/15.
+//  Copyright © 2015 Jan Brinkmann. All rights reserved.
+//
+
+import UIKit
+
+class RezeptCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
